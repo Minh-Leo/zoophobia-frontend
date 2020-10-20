115bf852799d404399e1595d33d4f8f6f4c9b637
Minh Bui <tranduyminh.bui@gmail.com>
2020-10-20 19:03:55 +1100

    patched security issues github

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // import Landing from './Landing';
 // import Game from './Game';
 // import CreateADeck from './CreateADeck';
