1c74cc08981f8c9c323d0fc763fee3cbee889608
Minh Bui <tranduyminh.bui@gmail.com>
2020-12-13 22:53:21 +1100

    ready for deploy

--- src/socketConfig.js
+++ src/socketConfig.js
@@ -2,6 +2,6 @@ import io from 'socket.io-client';
 
 const zoophobia = 'https://zoopobia.herokuapp.com/';
 
-const socket = io('http://localhost:3001');
-// const socket = io(zoophobia);
+// const socket = io('http://localhost:3001');
+const socket = io(zoophobia);
 export default socket;
